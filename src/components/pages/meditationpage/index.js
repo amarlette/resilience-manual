1655bfa4367ee96bd1f11e870fb88c11f22b1792
nehadesh <neha.n.deshpande@gmail.com>
2020-04-20 22:57:17 -0400

    added audio player test

--- src/components/pages/meditationpage/index.js
+++ src/components/pages/meditationpage/index.js
@@ -18,7 +18,7 @@ import {
     Responsive,
     Segment,
     Sidebar,
-    Visibility
+    Visibility, 
 } from "semantic-ui-react";
 
 import {
@@ -31,12 +31,17 @@ import {
 } from "react-router-dom";
 
 import ResponsiveContainer from "../../containers/responsive-container";
+import Audio from "../../audio-player";
+
 
 import Kaban from "./img/kaban.png";
 import TimeBlock from "./img/timeblocking-example.png";
 import Seinfeld from "./img/seinfeld.png";
+import MeditationLogo from "./img/gt-meditation-logo.png";
+
 
 export class MentalMaintenancepage extends React.Component {
+    
 
     render() {
         return (
@@ -46,16 +51,23 @@ export class MentalMaintenancepage extends React.Component {
                     <Header as="h2">What is mindfulness? </Header><br></br>
 
                     <ul>
-                        <li>Mindfulness  - maintaining a moment-by-moment awareness of our thoughts, feelings, bodily sensations, and surrounding environment, through a gentle, nurturing lens. When we practice mindfulness, our thoughts tune into what we're sensing in the present moment rather than rehashing the past or imagining the future.</li>
-                        <li>Mindfulness is that place in the middle, where we are neither consumed by nor reject what is going on. We can be fully present such that we’re interested in what’s going on, but we’re not forming judgments or dashing off into proliferation of thoughts. By being with what is, we can create a space where creativity arises, where other options arise. </li>
-                        <li>
-                        Mindfulness is linked to changes in the production of hormones and other chemicals that impact our physical health.   
-                        </li>
+                        <li>Mindfulness is maintaining a moment-by-moment awareness of our thoughts, feelings, bodily sensations, and surrounding environment, through a gentle, nurturing lens. When we practice mindfulness, our thoughts tune into what we're sensing in the present moment rather than rehashing the past or imagining the future.</li>
+                        <li>We neither consumed by nor reject what is going on. Being mindful allows us to create a space where creativity arises. </li>
                         <li>Scientists postulate that the development of mindfulness leads to non-judgmental and non-reactive acceptance of experience, which is associated with positive psychological and physical outcomes. </li>
                         <li>Mindfulness may lessen emotional experience of pain - In meditators, the region of the brain associated with unpleasantness of pain is less connected than normal to the prefrontal cortex (where emotions are processed) </li>
                         <li>Mindfulness may lessen fearful responses - meditation reduces the density of neurons and hence the activity in the amygdala and increases neuron density in the prefrontal cortex, which is an important area for regulating emotions. The reactive fear center of the brain shrinks, and the more thoughtful response center of the brain grows.</li>
                     </ul>
-            
+                    <Header as="h1">GT Meditation Club</Header><br></br>
+                    <Image src={MeditationLogo} alt="GT Meditation Club Logo"/>
+                    The purpose of the Meditation Club is to promote the well-being of Tech students, faculty, and staff through the practice of meditation. One goal includes members learning mindfulness and meditation techniques to relax, focus, and connect with oneself on a deeper level. We provide the space and time learn about different types of meditation practices that exist. All of our meetings are free and open for anyone to attend.
+                    <br></br>Find out more about us at: <a href="https://www.facebook.com/groups/GTMeditationClub/">https://www.facebook.com/groups/GTMeditationClub/</a>
+                    <br></br>
+                    We have collaborated with the GT Meditation Club to get you two sample meditation sessions that you can follow along with anytime you need it! 
+                    <Header as="h3">Mindfulness Meditation Practice (10 minutes)</Header><br></br>
+                        <div id="audio_div"></div>
+                    <Header as="h3">Mindfulness Breathing Meditation Practice (15 minutes)</Header><br></br>
+
+                    <Audio></Audio>
                 </Container>
 
                 <Segment inverted vertical style={{ marginTop: "4em", padding: "3em" }}>
